35ffc443097cb08fd61c908081b261d62a1eee8b
Dmitrii Ziablikov <dmitrii.ziablikov@spbpu.com>
2019-02-22 02:51:32 +0300

    - added animation

--- SimpleAppRouter/Routing/AppRouter.swift
+++ SimpleAppRouter/Routing/AppRouter.swift
@@ -25,9 +25,24 @@ final class Router {
             let wrappedVC = wrapInNavigationController(vc: self.rootController)
             self.currentViewController = self.rootController
             self.navigationController = wrappedVC
-            self.window?.rootViewController = wrappedVC
-            self.window?.makeKeyAndVisible()
+            setRootViewController(wrappedVC)
         }
+    }
+    
+    private func setRootViewController(_ vc: UIViewController, animated: Bool = true) {
+        guard animated, let window = self.window else {
+            self.window?.rootViewController = vc
+            self.window?.makeKeyAndVisible()
+            return
+        }
+        
+        window.rootViewController = vc
+        window.makeKeyAndVisible()
+        UIView.transition(with: window,
+                          duration: 0.3,
+                          options: .transitionCrossDissolve,
+                          animations: nil,
+                          completion: nil)
     }
 }
 
